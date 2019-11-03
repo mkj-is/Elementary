33bc1b934a9ac14911ace125c789681f4fa944c1
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-11-03 23:35:17 +0100

    Add function to combine effects

---
+++ Sources/Elementary/CombineEffects.swift
@@ -0,0 +1,7 @@
+public func combine<State, Action>(effects: Effect<State, Action>...) -> Effect<State, Action> {
+    { state, action, dispatch in
+        effects.forEach { effect in
+            effect(state, action, dispatch)
+        }
+    }
+}
