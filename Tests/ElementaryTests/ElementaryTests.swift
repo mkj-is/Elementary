15bc8f0fd200f3941f75549d915063bdf1811170
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-11-03 23:34:16 +0100

    Remove mockup tests

--- Tests/ElementaryTests/ElementaryTests.swift
+++ Tests/ElementaryTests/ElementaryTests.swift
@@ -9,29 +9,6 @@ final class ElementaryTests: XCTestCase {
         //XCTAssertEqual(Elementary().text, "Hello, World!")
     }
 
-    func testArrayStatePerformance() {
-        struct Todo {
-            let text = "Some todo"
-        }
-
-        enum TodoAction {
-            case add(Todo)
-        }
-
-        let store = TestStore<[Todo], TodoAction>(state: []) { state, action in
-            switch action {
-            case .add(let todo):
-                return state + [todo]
-            }
-        }
-
-        measure {
-            for _ in 0...1000 {
-                store.dispatch(TodoAction.add(Todo()))()
-            }
-        }
-    }
-
     static var allTests = [
         ("testExample", testExample),
     ]
