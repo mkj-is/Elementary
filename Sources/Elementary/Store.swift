76193f182a285ccc0fa612aa02b0aa6115cf9b25
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-11-03 23:35:04 +0100

    Add store implementation

--- Sources/Elementary/Store.swift
+++ Sources/Elementary/Store.swift
@@ -1,6 +1,24 @@
 
-public protocol Store {
-    associatedtype Action
+public final class Store<State, Action> {
+    public private(set) var state: State
+    private let update: Update<State, Action>
+    private let effect: Effect<State, Action>?
 
-    func dispatch(_ action: Action) -> Call
+    public init(
+        state: State,
+        update: @escaping Update<State, Action>,
+        effect: Effect<State, Action>? = nil,
+        initialAction: Action? = nil
+    ) {
+        self.state = state
+        self.update = update
+        self.effect = effect
+
+        initialAction.flatMap(dispatch)
+    }
+
+    public func dispatch(_ action: Action) {
+        update(&state, action)
+        effect?({ self.state }, action, dispatch)
+    }
 }
