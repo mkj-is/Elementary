e5eceadf415649888b356ccee2438fe66a08974c
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-11-04 11:32:59 +0100

    Add combine for arrays

--- Sources/Elementary/CombineEffects.swift
+++ Sources/Elementary/CombineEffects.swift
@@ -1,4 +1,8 @@
 public func combine<State, Action>(effects: Effect<State, Action>...) -> Effect<State, Action> {
+    combine(effects: effects)
+}
+
+public func combine<State, Action>(effects: [Effect<State, Action>]) -> Effect<State, Action> {
     { state, action, dispatch in
         effects.forEach { effect in
             effect(state, action, dispatch)
