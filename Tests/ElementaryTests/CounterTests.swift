c345ab287899121d97cb0ce8118864d3636f47c2
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2020-02-12 21:40:36 +0100

    Add basic counter app integration tests

---
+++ Tests/ElementaryTests/CounterTests.swift
@@ -0,0 +1,58 @@
+import XCTest
+import Elementary
+
+final class CounterTests: XCTestCase {
+
+    func testIncrement() {
+        let store = counterStore()
+        XCTAssertEqual(0, store.state)
+        store.dispatch(.increment)
+        XCTAssertEqual(1, store.state)
+    }
+
+    func testDecrement() {
+        let store = counterStore(state: 5)
+        XCTAssertEqual(5, store.state)
+        store.dispatch(.decrement)
+        XCTAssertEqual(4, store.state)
+    }
+
+    func testDecrementWhenZero() {
+        let store = counterStore()
+        XCTAssertEqual(0, store.state)
+        store.dispatch(.decrement)
+        XCTAssertEqual(0, store.state)
+    }
+
+    func testReset() {
+        let store = counterStore(state: 5)
+        XCTAssertEqual(5, store.state)
+        store.dispatch(.reset)
+        XCTAssertEqual(0, store.state)
+    }
+
+    func testActionSequence() {
+        let store = counterStore()
+        XCTAssertEqual(0, store.state)
+        store.dispatch(.increment)
+        XCTAssertEqual(1, store.state)
+        store.dispatch(.increment)
+        XCTAssertEqual(2, store.state)
+        store.dispatch(.increment)
+        XCTAssertEqual(3, store.state)
+        store.dispatch(.decrement)
+        XCTAssertEqual(2, store.state)
+        store.dispatch(.increment)
+        XCTAssertEqual(3, store.state)
+        store.dispatch(.reset)
+        XCTAssertEqual(0, store.state)
+    }
+
+    static var allTests = [
+        ("testIncrement", testIncrement),
+        ("testDecrement", testDecrement),
+        ("testDecrementWhenZero", testDecrementWhenZero),
+        ("testReset", testReset),
+        ("testActionSequence", testActionSequence),
+    ]
+}
---
+++ Tests/ElementaryTests/Mockups/Counter.swift
@@ -0,0 +1,24 @@
+import Elementary
+
+enum CounterAction {
+    case increment, decrement, reset
+}
+
+
+func updateCounter(state: inout Int, action: CounterAction) {
+    switch action {
+    case .increment:
+        state += 1
+    case .decrement:
+        state = max(0, state - 1)
+    case .reset:
+        state = 0
+    }
+}
+
+func counterStore(state: Int = 0) -> Store<Int, CounterAction> {
+    Store(
+        state: state,
+        update: updateCounter
+    )
+}
