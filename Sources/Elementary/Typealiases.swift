07b202808e19ee0bc0535989ab480ae27d8abc85
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2020-02-12 21:20:53 +0100

    Unify void return types

--- Sources/Elementary/Typealiases.swift
+++ Sources/Elementary/Typealiases.swift
@@ -1,4 +1,4 @@
 public typealias Call = () -> Void
-public typealias Dispatch<Action> = (Action) -> ()
+public typealias Dispatch<Action> = (Action) -> Void
 public typealias Update<State, Action> = (inout State, Action) -> Void
-public typealias Effect<State, Action> = (@escaping () -> State, Action, @escaping Dispatch<Action>) -> ()
+public typealias Effect<State, Action> = (@escaping () -> State, Action, @escaping Dispatch<Action>) -> Void
