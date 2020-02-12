7f78c8ff5fee7776a875fdad965c9fdacc69843c
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2020-02-12 19:19:00 +0100

    Rename files

---
+++ Sources/Elementary/Effect+Join.swift
@@ -0,0 +1,11 @@
+public func join<State, Action>(effects: Effect<State, Action>...) -> Effect<State, Action> {
+    join(effects: effects)
+}
+
+public func join<State, Action>(effects: [Effect<State, Action>]) -> Effect<State, Action> {
+    { state, action, dispatch in
+        effects.forEach { effect in
+            effect(state, action, dispatch)
+        }
+    }
+}
